db92b6259558fbcd93fe91fe0308ff9db28a0e9e
thatcoderguy <davie@recallhosting.co.uk>
2014-08-12 17:57:55 +0100

    Commit of fresh MVC5 project

---
+++ Secure Password Repository/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Secure_Password_Repository.Startup))]
+namespace Secure_Password_Repository
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}

5a29160ceba20b5d448d96880beaae911430cda5
widagroup <davie@widagroup.com>
2014-08-21 13:28:18 +0100

    Test Commit

--- Secure Password Repository/Scripts/categoryandpassword.js
+++ Secure Password Repository/Scripts/categoryandpassword.js
@@ -1,6 +1,4 @@
 ﻿
-//<div><i class="glyphicon glyphicon-folder-open rightfolderpadding"></i>Category 3<span class="btn-group"><a href="@Url.Action("Edit", "0")" class="btn btn-xs btn-primary"><i class="glyphicon glyphicon-edit"></i>Edit</a><a href="@Url.Action("Delete", "0")" class="btn btn-xs btn-danger"><i class="glyphicon glyphicon-remove"></i>Delete</a></span></div>
-
 function createNewCategory(holderid) {
 
     var random = parseInt(Math.random() * 1000).toString();

6992f673ce28cbe39388446b7689caad20325f7d
thatcoderguy <davie@recallhosting.co.uk>
2014-09-09 07:45:07 +0100

    Started to implement global error handler

--- Secure Password Repository/Scripts/extensions.js
+++ Secure Password Repository/Scripts/extensions.js
@@ -1,4 +1,15 @@
-﻿//add the CSRF token to AJAX requests
+﻿
+//add the CSRF token to AJAX requests
 AddAntiForgeryToken = function (data) {
     data.__RequestVerificationToken = $('#_CRSFform input[name=__RequestVerificationToken]').val(); return data;
 };
+
+$(document).ajaxError(function( event, jqxhr, settings, thrownError ) {
+    if(thrownError=='Unauthorised') {
+        window.location = '/';
+    }
+}
+
+//AJAX FAIL
+
+//AJAX ERROR

bef5ee26cbe82c534b1f87e365bf3adca67fd42a
widagroup <davie@widagroup.com>
2014-10-01 13:10:32 +0100

    Rmeoved all references to base64 convertion in the encryption methods + added type extensions for base64 conversion

--- Secure Password Repository/Extensions/CustomPasswordHasher.cs
+++ Secure Password Repository/Extensions/CustomPasswordHasher.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Web;
 
 namespace Secure_Password_Repository.Extensions
@@ -20,25 +21,31 @@ namespace Secure_Password_Repository.Extensions
         public string HashPassword(string Password)
         {
             if (Password == null)
-                throw new ArgumentNullException("password");
+                throw new ArgumentNullException("Missing Password");
             else
                 //generate a HMAC using the PBKDF2 method
-                return EncryptionAndHashing.Hash_PBKDF2(Password);
+                return EncryptionAndHashing.Hash_PBKDF2(Password).ConvertToBase64();
         }
 
         public PasswordVerificationResult VerifyHashedPassword(string hashedPassword, string providedPassword)
         {
-            if (hashedPassword == null)
-                throw new ArgumentNullException("password");
+            if (hashedPassword == null || hashedPassword.Length ==0)
+                throw new ArgumentNullException("Missing Password");
 
-            if (providedPassword == null)
-                throw new ArgumentNullException("password");
+            if (providedPassword == null || providedPassword.Length == 0)
+                throw new ArgumentNullException("Missing Password");
+
+            //convert the stored hash from base64
+            hashedPassword = hashedPassword.ConvertFromBase64();
 
             //grab the original salt used to generate the HMAC
             string salt = EncryptionAndHashing.Get_PBKDF2Salt(hashedPassword);
 
+            //generate a hash to compare based on the user's password and the salt for the stored password
+            providedPassword = EncryptionAndHashing.Hash_PBKDF2(providedPassword, salt);
+
             //now use the salt and the plain-text password to generate a HMAC and verify
-            if (hashedPassword.Equals(EncryptionAndHashing.Hash_PBKDF2(providedPassword, salt, true)))
+            if (hashedPassword.Equals(providedPassword))
                 return PasswordVerificationResult.Success;
             else
                 return PasswordVerificationResult.Failed;

19a587e9e549fb3aa2965d4b0fbf91e5a3601733
thatcoderguy <davie@recallhosting.co.uk>
2014-11-05 07:43:08 +0000

    Decryption of user key should now be working - encrypting V1 passwords now needs to be implemented.

--- V1 Import Tool/Form1.Designer.cs
+++ V1 Import Tool/Form1.Designer.cs
@@ -28,12 +28,221 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.txtInstance = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtV2DBName = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtV1DBName = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtUsername = new System.Windows.Forms.TextBox();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtSalt = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtIterationCount = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txtIV = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(86, 213);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(123, 33);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtInstance
+            // 
+            this.txtInstance.Location = new System.Drawing.Point(110, 9);
+            this.txtInstance.Name = "txtInstance";
+            this.txtInstance.Size = new System.Drawing.Size(188, 20);
+            this.txtInstance.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(66, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Sql Instance";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 60);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(69, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "V2 DB Name";
+            // 
+            // txtV2DBName
+            // 
+            this.txtV2DBName.Location = new System.Drawing.Point(110, 56);
+            this.txtV2DBName.Name = "txtV2DBName";
+            this.txtV2DBName.Size = new System.Drawing.Size(188, 20);
+            this.txtV2DBName.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 37);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(69, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "V1 DB Name";
+            // 
+            // txtV1DBName
+            // 
+            this.txtV1DBName.Location = new System.Drawing.Point(110, 33);
+            this.txtV1DBName.Name = "txtV1DBName";
+            this.txtV1DBName.Size = new System.Drawing.Size(188, 20);
+            this.txtV1DBName.TabIndex = 5;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(13, 84);
+            this.label4.Name = "label4";
+            this.label4.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.label4.Size = new System.Drawing.Size(71, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "V2 Username";
+            // 
+            // txtUsername
+            // 
+            this.txtUsername.Location = new System.Drawing.Point(110, 80);
+            this.txtUsername.Name = "txtUsername";
+            this.txtUsername.Size = new System.Drawing.Size(188, 20);
+            this.txtUsername.TabIndex = 8;
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(110, 103);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.PasswordChar = '*';
+            this.txtPassword.Size = new System.Drawing.Size(188, 20);
+            this.txtPassword.TabIndex = 10;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(13, 107);
+            this.label5.Name = "label5";
+            this.label5.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.label5.Size = new System.Drawing.Size(69, 13);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "V2 Password";
+            // 
+            // txtSalt
+            // 
+            this.txtSalt.Location = new System.Drawing.Point(110, 129);
+            this.txtSalt.Name = "txtSalt";
+            this.txtSalt.PasswordChar = '*';
+            this.txtSalt.Size = new System.Drawing.Size(188, 20);
+            this.txtSalt.TabIndex = 12;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(13, 133);
+            this.label6.Name = "label6";
+            this.label6.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.label6.Size = new System.Drawing.Size(41, 13);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "V2 Salt";
+            this.label6.Click += new System.EventHandler(this.label6_Click);
+            // 
+            // txtIterationCount
+            // 
+            this.txtIterationCount.Location = new System.Drawing.Point(110, 155);
+            this.txtIterationCount.Name = "txtIterationCount";
+            this.txtIterationCount.PasswordChar = '*';
+            this.txtIterationCount.Size = new System.Drawing.Size(188, 20);
+            this.txtIterationCount.TabIndex = 14;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(13, 159);
+            this.label7.Name = "label7";
+            this.label7.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.label7.Size = new System.Drawing.Size(92, 13);
+            this.label7.TabIndex = 13;
+            this.label7.Text = "V2 Iteration Count";
+            // 
+            // txtIV
+            // 
+            this.txtIV.Location = new System.Drawing.Point(110, 181);
+            this.txtIV.Name = "txtIV";
+            this.txtIV.PasswordChar = '*';
+            this.txtIV.Size = new System.Drawing.Size(188, 20);
+            this.txtIV.TabIndex = 16;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(14, 181);
+            this.label8.Name = "label8";
+            this.label8.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.label8.Size = new System.Drawing.Size(33, 13);
+            this.label8.TabIndex = 15;
+            this.label8.Text = "V2 IV";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(304, 258);
+            this.Controls.Add(this.txtIV);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.txtIterationCount);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.txtSalt);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtUsername);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtV1DBName);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtV2DBName);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtInstance);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txtInstance;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtV2DBName;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtV1DBName;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtUsername;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtSalt;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtIterationCount;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox txtIV;
+        private System.Windows.Forms.Label label8;
     }
 }
 
--- V1 Import Tool/Form1.cs
+++ V1 Import Tool/Form1.cs
@@ -7,14 +7,70 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
+using V1_Import_Tool.Extensions;
 
 namespace V1_Import_Tool
 {
     public partial class Form1 : Form
     {
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            SqlConnection version1dbconnection = new SqlConnection("Data Source=" + txtInstance.Text + ";Initial Catalog=" + txtV1DBName.Text + ";Integrated Security=SSPI");
+            SqlConnection version2dbconnection = new SqlConnection("Data Source=" + txtInstance.Text + ";Initial Catalog=" + txtV2DBName.Text + ";Integrated Security=SSPI");
+
+            version1dbconnection.Open();
+            version2dbconnection.Open();
+
+            EncryptionAndHashing objEncryption = new EncryptionAndHashing();
+
+            objEncryption.systemIterationCount = txtIterationCount.Text;
+            objEncryption.systemIV = txtIV.Text;
+            objEncryption.systemSalt = txtSalt.Text;
+
+            byte[] privateKey;
+            byte[] encryptionKey;
+
+            //first retrieve DB key
+            SqlCommand sqlcomm = new SqlCommand("select userPrivateKey, userEncryptionKey FROM AspNetUsers WHERE UserName = '" + txtUsername.Text + ';');
+            SqlDataReader sqlreader = sqlcomm.ExecuteReader();
+            
+            if(sqlreader.HasRows)
+            {
+                sqlreader.Read();
+                privateKey = sqlreader["userPrivateKey"].ToString().ToBytes();
+                encryptionKey = sqlreader["userEncryptionKey"].ToString().ToBytes();
+
+                //hash and encrypt the user's password - so this can be used to decrypt the user's private key
+                byte[] hashedPassword = objEncryption.Hash_SHA1_ToBytes(txtPassword.Text);
+                hashedPassword = objEncryption.Hash_PBKDF2_ToBytes(hashedPassword, txtSalt.Text).ToBase64();
+
+                //decrypt the user private key
+                privateKey = objEncryption.Decrypt_AES256_ToBytes(privateKey, hashedPassword).FromBase64();
+
+                //decrypt the user's copy of the password encryption key
+                encryptionKey = objEncryption.Decrypt_RSA_ToBytes(encryptionKey, privateKey);
+            }
+
+            sqlreader.Close();
+            sqlreader.Dispose();
+
+            string data = objEncryption.Encrypt_AES256_ToBytes(DataToEncrypt.ToBase64(), encryptionKey).ToBase64();
+
+
+
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
